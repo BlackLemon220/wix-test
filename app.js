8b9293919dc6a77c4e4806b843254175adeb5367
Yinon Cohen <yinonmail@gmail.com>
2017-10-18 02:43:59 +0300

    Fixed the structure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-reddit.hot('aww').limit(5).fetch(function(res) {
+/*reddit.hot('aww').limit(5).fetch(function(res) {
 // res contains JSON parsed response from Reddit
   console.log(res.data.children[0].data.preview.images[0].resolutions[0].url);
   document.getElementById("pic1").src = res.data.children[1].data.preview.images[0].resolutions[0].url;
