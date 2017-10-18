f736b9e447ba18504ae216fe62a681ca3e5f07a7
Yinon Cohen <yinonmail@gmail.com>
2017-10-18 18:14:20 +0300

    Pagination works, need counter pages

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var defaultSubreddit = "aww";
 var redditcom = "https://www.reddit.com";
-searchHandler(defaultSubreddit);
+var currentCategory = "Hot";
+var currentSub = defaultSubreddit;
+var i = 1;
+var currentPage = 1;
+var maxPage = 5;
+searchHandler();
 
 function showSub(sub) {
     var opt = document.getElementById("select");
-    var category = opt.options[opt.selectedIndex].value;
+    currentCategory = opt.options[opt.selectedIndex].value;
     var c;
-    switch(category){
+    switch(currentCategory){
         case 'Hot':
             c = reddit.hot(sub);
             break;
@@ -22,21 +27,72 @@ function showSub(sub) {
     }
     c.limit(13).fetch(function(res) {
         // res contains JSON parsed response from Reddit
-        for(var i = 1; i <= 12; i++) {
-            var myData = res.data.children[i].data;
+        for(var j = 1; j <= 12; j++) {
+            var myData = res.data.children[j].data;
             var pic = decodeHtml(myData.preview.images[0].resolutions[1].url);
-            var picID = "pic" + i;
-            var aID = "a" + i;
+            var picID = "pic" + j;
+            var aID = "a" + j;
             var permalink = myData.permalink;
             var ref = redditcom + permalink;
             document.getElementById(picID).src = pic;
             document.getElementById(picID).title = myData.title;
             document.getElementById(aID).href = ref;
+            i++;
         }
     });
 }
 
-function searchHandler(sub) {
-    sub = document.getElementById('search-text').value || sub;
-    showSub(sub);
+function showPage(sub, category) {
+    switch(category){
+        case 'Hot':
+            c = reddit.hot(sub);
+            break;
+        case 'Top':
+            c = reddit.top(sub);
+            break;
+        case 'New':
+            c = reddit.new(sub);
+            break;
+        default:
+            c = reddit.hot(sub);
+            break;
+    }
+    var limit = (12 * (currentPage)) + 1;
+    c.limit(limit).fetch(function(res) {
+        // res contains JSON parsed response from Reddit
+        for(var j = 1; j <= 12; j++) {
+            var myData = res.data.children[i].data;
+            var pic = decodeHtml(myData.preview.images[0].resolutions[1].url);
+            var picID = "pic" + j;
+            var aID = "a" + j;
+            var permalink = myData.permalink;
+            var ref = redditcom + permalink;
+            document.getElementById(picID).src = pic;
+            document.getElementById(picID).title = myData.title;
+            document.getElementById(aID).href = ref;
+            i++;
+        }
+    });
+}
+
+function paginationNext() {
+    if(currentPage !== 5) {
+        currentPage++;
+        showPage(currentSub, currentCategory);
+    }
+}
+function paginationBack() {
+    if(currentPage !== 1) {
+        currentPage--;
+        i = ((currentPage - 1) * 12) + 1;
+        showPage(currentSub, currentCategory);
+    }
+}
+
+
+function searchHandler() {
+    i = 1;
+    currentPage = 1;
+    currentSub = document.getElementById('search-text').value || currentSub;
+    showSub(currentSub);
 }
