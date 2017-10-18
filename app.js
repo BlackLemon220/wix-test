19be59fc408b98697c5ebc548602b9a976ec55fe
Yinon Cohen <yinonmail@gmail.com>
2017-10-18 12:34:32 +0300

    Finished showing pictures

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-/*reddit.hot('aww').limit(5).fetch(function(res) {
+reddit.hot('aww').limit(12).fetch(function(res) {
 // res contains JSON parsed response from Reddit
-  console.log(res.data.children[0].data.preview.images[0].resolutions[0].url);
-  document.getElementById("pic1").src = res.data.children[1].data.preview.images[0].resolutions[0].url;
+  //console.log(res.data.children[0].data.preview.images[0].resolutions[0].url);
+    for(var i = 1; i <= 12; i++) {
+        var pic = decodeHtml(res.data.children[i].data.preview.images[0].resolutions[1].url);
+        var picID = "pic" + i;
+        var aID = "a" + i;
+        document.getElementById(picID).src = pic;
+        document.getElementById(picID).title = res.data.children[i].data.title;
+        var permalink = res.data.children[i].data.permalink;
+        var ref = "https://www.reddit.com" + permalink;
+        document.getElementById(aID).href = ref;
+    }
+    console.log(res.data.children[1].data);
 });
+
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
