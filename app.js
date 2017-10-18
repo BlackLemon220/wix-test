cbfb43616a8f649754e4fb6e3d9d8ec763c4713d
Yinon Cohen <yinonmail@gmail.com>
2017-10-18 16:30:39 +0300

    Finished choosing by category

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-var defaultSub = "aww";
+var defaultSubreddit = "aww";
 var redditcom = "https://www.reddit.com";
-showSub(defaultSub);
+searchHandler(defaultSubreddit);
+
 function showSub(sub) {
-    reddit.top(sub).limit(13).fetch(function(res) {
+    var opt = document.getElementById("select");
+    var category = opt.options[opt.selectedIndex].value;
+    var c;
+    switch(category){
+        case 'Hot':
+            c = reddit.hot(sub);
+            break;
+        case 'Top':
+            c = reddit.top(sub);
+            break;
+        case 'New':
+            c = reddit.new(sub);
+            break;
+        default:
+            c = reddit.hot(sub);
+            break;
+    }
+    c.limit(13).fetch(function(res) {
         // res contains JSON parsed response from Reddit
-        console.log(res);
         for(var i = 1; i <= 12; i++) {
-            var pic = decodeHtml(res.data.children[i].data.preview.images[0].resolutions[1].url);
+            var myData = res.data.children[i].data;
+            var pic = decodeHtml(myData.preview.images[0].resolutions[1].url);
             var picID = "pic" + i;
             var aID = "a" + i;
-            document.getElementById(picID).src = pic;
-            document.getElementById(picID).title = res.data.children[i].data.title;
-            var permalink = res.data.children[i].data.permalink;
+            var permalink = myData.permalink;
             var ref = redditcom + permalink;
+            document.getElementById(picID).src = pic;
+            document.getElementById(picID).title = myData.title;
             document.getElementById(aID).href = ref;
         }
-        });
+    });
 }
 
-function searchHandler() {
-    var sub = document.getElementById('search-text').value;
+function searchHandler(sub) {
+    sub = document.getElementById('search-text').value || sub;
     showSub(sub);
 }
