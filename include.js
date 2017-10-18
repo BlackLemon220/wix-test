f7c47efc2312fa50f6b5054b1accb5c152d32ecd
Yinon Cohen <yinonmail@gmail.com>
2017-10-18 15:39:03 +0300

    Added select box

--- include.js
+++ include.js
@@ -3,17 +3,3 @@ function decodeHtml(html) {
     txt.innerHTML = html;
     return txt.value;
 }
-
-
-
-function UrlExists(url, cb){
-    jQuery.ajax({
-        url:      url,
-        dataType: 'text',
-        type:     'GET',
-        complete:  function(xhr){
-            if(typeof cb === 'function')
-               cb.apply(this, [xhr.status]);
-        }
-    });
-}
