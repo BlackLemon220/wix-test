2e238567816b4b92d1a64bed5a04a40df2e5cedb
Yinon Cohen <yinonmail@gmail.com>
2017-10-19 13:08:38 +0300

    Code improvement

--- js/app.js
+++ js/app.js
@@ -2,31 +2,46 @@ var defaultSubreddit = "aww";
 var redditcom = "https://www.reddit.com";
 var currentCategory = "Hot";
 var currentSub = defaultSubreddit;
-var i = 1;
 var currentPage = 1;
 var maxPage = 5;
 var imageNumber = 12;
+var firstImage;
+var lastImage;
 searchHandler();
 
-function showSub(sub, categorty, limit) {
+/**
+ * showSub changes all images according to the parameters.
+ * @param {string} sub           [subreddit. By defatult was set to "aww"]
+ * @param {string} categorty     [The select/option value. By default was set to "Hot"]
+ * @param {string} beforeOrAfter [Works when pagination activates, value of "after" and "before.
+ *                               Otherwize, undefined.]
+ * @param {string} imageCode     [Works when pagination activates, value of first / last image of last run.
+ *                               Otherwize, undefined.]
+ */
+function showSub(sub, categorty, beforeOrAfter, imageCode) {
     var c;
+    // Although "Hot" is by default, i've added it to a 'case' just for more readable code.
     switch(categorty){
         case 'Hot':
-            c = reddit.hot(sub);
+            c = reddit.hot(sub).limit(imageNumber + 1);
             break;
         case 'Top':
-            c = reddit.top(sub);
+            c = reddit.top(sub).limit(imageNumber + 1);
             break;
         case 'New':
-            c = reddit.new(sub);
+            c = reddit.new(sub).limit(imageNumber + 1);
             break;
         default:
-            c = reddit.hot(sub);
+            c = reddit.hot(sub).limit(imageNumber + 1);
             break;
     }
-    c.limit(limit).fetch(function(res) {
+    // if / else if statement for pagination:
+    if(beforeOrAfter === "after") c = c.after(imageCode);
+    else if(beforeOrAfter === "before") c = c.before(imageCode);
+    c.fetch(function(res) {
+        // Loop for all 12 images, getting their urls from res object.
         for(var j = 1; j <= imageNumber; j++) {
-            var myData = res.data.children[i].data;
+            var myData = res.data.children[j].data;
             var pic = decodeHtml(myData.preview.images[0].resolutions[1].url);
             var picID = "pic" + j;
             var aID = "a" + j;
@@ -35,34 +50,40 @@ function showSub(sub, categorty, limit) {
             document.getElementById(picID).src = pic;
             document.getElementById(picID).title = myData.title;
             document.getElementById(aID).href = ref;
-            i++;
+            if(j === 1) firstImage = myData.name;
+            if(j === imageNumber) lastImage = myData.name;
         }
     });
 }
+/**
+ * Activates when "Next" button is clicked.
+ */
 function paginationNext() {
     if(currentPage !== maxPage) {
         currentPage++;
-        var limit = (imageNumber * (currentPage)) + 1;
-        showSub(currentSub, currentCategory, limit);
+        showSub(currentSub, currentCategory, "after", lastImage);
         document.getElementById("numberPage").innerHTML = currentPage + " / " + maxPage;
     }
 }
+/**
+ * Activates when "Back" button is clicked.
+ */
 function paginationBack() {
     if(currentPage !== 1) {
         currentPage--;
-        i = ((currentPage - 1) * imageNumber) + 1;
-        var limit = (imageNumber * (currentPage)) + 1;
-        showSub(currentSub, currentCategory, limit);
+        showSub(currentSub, currentCategory, "before", firstImage);
         document.getElementById("numberPage").innerHTML = currentPage + " / " + maxPage;
     }
 }
+/**
+ * Activates when search button is clicked. On startup, it being.
+ * 'currentSub' gets its value from the search input box. If empty, gets its last \ default sub.
+ */
 function searchHandler() {
-    i = 1;
     currentPage = 1;
     currentSub = document.getElementById('search-text').value || currentSub;
     var opt = document.getElementById("select");
     currentCategory = opt.options[opt.selectedIndex].value;
-    var limit = (i * imageNumber) + 1;
-    showSub(currentSub, currentCategory, limit);
+    showSub(currentSub, currentCategory);
     document.getElementById("numberPage").innerHTML = currentPage + " / " + maxPage;
 }
